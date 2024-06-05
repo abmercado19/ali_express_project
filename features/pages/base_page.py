99f86370679e2588c09bfdf9f53d09147cdda5d8
Ana <abmercado19@gmail.com>
2024-06-05 20:22:23 -0300

    Implementing step and page object to search a product

--- features/pages/base_page.py
+++ features/pages/base_page.py
@@ -1,8 +1,25 @@
+import logging
+from selenium.webdriver.common.by import By
+
 
 class BasePage:
 
+    subscription_popup_close_button = (By.XPATH,
+                                       "//div[./div[contains(text(),'notificaciones')]]//following-sibling::img")
+
     def __init__(self, driver):
+        logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
         self.driver = driver
 
     def open_aliexpress_page(self):
         self.driver.get("https://www.aliexpress.com/")
+
+    def close_suscription_popup_if_present(self):
+        try:
+            close_button = self.driver.find_element(*self.subscription_popup_close_button)
+            close_button.click()
+            logging.info("Subscription popup was closed.")
+        except:
+            logging.info("Subscription popup is not displayed.")
+
+
