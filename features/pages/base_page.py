7d01c9cbc5b843797af3c8e9de9430be7ca5cf94
Ana <abmercado19@gmail.com>
2024-06-05 22:25:27 -0300

    Adding implementation to preview item details

--- features/pages/base_page.py
+++ features/pages/base_page.py
@@ -3,9 +3,9 @@ from selenium.webdriver.common.by import By
 
 
 class BasePage:
-
-    subscription_popup_close_button = (By.XPATH,
-                                       "//div[./div[contains(text(),'notificaciones')]]//following-sibling::img")
+    xpath_lower_text = "translate(text(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz')"
+    subscription_popup_close_button = (By.XPATH, "//div[./div[contains("
+                                       + xpath_lower_text + ",'notificaciones')]]//following-sibling::img")
 
     def __init__(self, driver):
         logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
--- features/pages/products_list_page.py
+++ features/pages/products_list_page.py
@@ -2,12 +2,18 @@ import logging
 
 from selenium.common import ElementClickInterceptedException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from features.pages.menu_page import MenuPage
 
 
 class ProductsListPage(MenuPage):
 
     page_button = (By.XPATH, "//li[contains(@class,'comet-pagination-item-{}')]")
+    products_list_view_as_list = (By.XPATH, "//span[contains(@class,'viewlist')]")
+    products_in_list = (By.XPATH, "//div[contains(@class,'card--out-wrapper')]")
+    previsualize_button = (By.XPATH, ".//div[" + MenuPage.xpath_lower_text + "='previsualizar']")
 
     def __init__(self, driver):
         super().__init__(driver)
@@ -21,6 +27,17 @@ class ProductsListPage(MenuPage):
             except ElementClickInterceptedException:
                 # Click on search button to ensure there is not any element intercepting the page button
                 self.click_search_field()
+
+    def select_product_by_position(self, product_position):
+        product_list_view_as_list = self.driver.find_element(*self.products_list_view_as_list)
+        product_list_view_as_list.click()
+        logging.info("List view selected")
+        product_in_list_by_position = self.driver.find_elements(*self.products_in_list)[int(product_position) - 1]
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.element_to_be_clickable(self.previsualize_button))
+        previsualize_button = product_in_list_by_position.find_element(*self.previsualize_button)
+        previsualize_button.click()
+        logging.info("Previsualize button clicked")
 
 
 
